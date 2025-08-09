0b04f4ec28eeb782cf5e62ef6833e16347316b56
Shiva <ShivaBala2015@gmail.com>
2025-08-09 15:31:31 -0400

    init

---
+++ frontend/src/config.js
@@ -0,0 +1,12 @@
+export const API_URL = "http://localhost:3000";
+
+// For local development
+// export const API_URL = "http://localhost:3000";
+// swapping the port for the port your agent is running on locally
+
+// For production
+// export const API_URL =
+//   "https://eaa34c584767fa9f48b41a3e8e8b85cdeee5bce4-3000.dstack-prod7.phala.network";
+// swapping the url for your Phala Cloud url
+
+// Make sure you don't have a trailing slash on the URL
